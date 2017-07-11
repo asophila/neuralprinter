93edb3e59eb7d317d4655100e00f2ccf637b66f9
Gabriel <dralion14@gmail.com>
2017-07-11 10:17:03 -0400

    try-catch en script

--- run_print.py
+++ run_print.py
@@ -24,48 +24,50 @@ else:
 s = sched.scheduler(time.time, time.sleep)
 
 def find_next_print(sc):
-    continuar = True
+    try:
+        continuar = True
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        r = requests.get('http://pinta.bicubi.co/get_next_to_print.php?evento=' + evento, headers=headers)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    r = requests.get('http://pinta.bicubi.co/get_next_to_print.php?evento=' + evento, headers=headers)
+        if r.status_code == 200:
+            next_print = r.json()
+            if next_print and not next_print['error']:
+                print('-----------------------------------')
+                r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_print['id'], 'status': 'IMPRIMIENDO'}, headers=headers)
+                print(str(time.time()), 'imprimiendo imagen', next_print['name'])
+                print('-----------------------------------')
 
-    if r.status_code == 200:
-        next_print = r.json()
-        if next_print and not next_print['error']:
-            print('-----------------------------------')
-            r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_print['id'], 'status': 'IMPRIMIENDO'}, headers=headers)
-            print(str(time.time()), 'imprimiendo imagen', next_print['name'])
-            print('-----------------------------------')
+                # guardar imagen a imprimir
+                filename = 'print/' + str(time.time()) + '_' + next_print['name'] + next_print['ext']
+                with open(filename, "wb") as fh:
+                    fh.write(base64.b64decode(next_print['imagen']))
 
-            # guardar imagen a imprimir
-            filename = 'print/' + str(time.time()) + '_' + next_print['name'] + next_print['ext']
-            with open(filename, "wb") as fh:
-                fh.write(base64.b64decode(next_print['imagen']))
+                # enviar por email
+                sender.email([next_print['correo']], [filename])
 
-            # enviar por email
-            sender.email([next_print['correo']], [filename])
+                # windows
+                codigo = next_print['codigo']
+                imprimir = True if codigo else False
+                print('Imprimir:', imprimir)
+                printer.print_image(filename, print_image = imprimir)
 
-            # windows
-            codigo = next_print['codigo']
-            imprimir = True if codigo else False
-            print('Imprimir:', imprimir)
-            printer.print_image(filename, print_image = imprimir)
+                # borrar imagenes
+                if not save_images:
+                    os.remove(filename)
 
-            # borrar imagenes
-            if not save_images:
-                os.remove(filename)
-
-            print('-----------------------------------')
-            r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_print['id'], 'status': 'IMPRESO'}, headers=headers)
-            print(str(time.time()), 'impresa imagen', next_print['name'])
-            print('-----------------------------------')
+                print('-----------------------------------')
+                r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_print['id'], 'status': 'IMPRESO'}, headers=headers)
+                print(str(time.time()), 'impresa imagen', next_print['name'])
+                print('-----------------------------------')
+            else:
+                print(str(time.time()), next_print['message'])
+                if 'evento' in next_print['message']:
+                    continuar = False
         else:
-            print(str(time.time()), next_print['message'])
-            if 'evento' in next_print['message']:
-                continuar = False
+            print(str(time.time()), 'Error al recibir respuesta desde el servidor')
 
-    else:
-        print(str(time.time()), 'Error al recibir respuesta desde el servidor')
+    except:
+        print('Error red?')
     # do your stuff
     s.enter(10, 1, find_next_print, (sc,))
 
