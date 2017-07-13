994213705da738a7698ac5f8fa3b5ad3b68f0330
Gabriel <dralion14@gmail.com>
2017-07-13 17:40:50 -0400

    mostrar si hay por procesar

---
+++ php/has_next_to_process.php
@@ -0,0 +1,36 @@
+<?php
+
+function get_next_to_process(){
+    $conn = get_conn();
+    $sql = "SELECT * FROM `image` WHERE `status` = 'A_PROCESAR' ORDER BY `id` ASC";
+
+    $result = $conn->query($sql);
+    $response = array();
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $response = array(
+            'error' => false,
+            'message' => '',
+            'id' => $row['id'],
+            'name' => $row['name'],
+            'ext' => $row['ext'],
+            'estilo' => $row['estilo'],
+            'evento' => $row['ip']
+        );
+    } else {
+        $response = array(
+            'error' => true,
+            'message' => 'No hay más por procesar'
+        );
+    }
+    $conn->close();
+    return json_encode($response);
+}
+
+header("Content-Type: application/json; charset=utf-8");
+include 'db.php';
+
+echo get_next_to_process();
+
+?>
--- run_process.py
+++ run_process.py
@@ -9,62 +9,77 @@ import urllib.request
 def find_next_process(sc):
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        # preguntar si hay nueva imagen
         r = requests.get('http://pinta.bicubi.co/get_next_to_process.php', headers=headers)
-
         if r.status_code == 200:
             next_style = r.json()
             if next_style and not next_style['error']:
                 print('-----------------------------------')
-                r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_style['id'], 'status': 'PROCESANDO'}, headers=headers)
-                print(str(time.time()), 'procesando imagen', next_style['name'])
-                print('-----------------------------------')
+                print('descargando imagen...')
 
-                model = 'model/' + next_style['estilo'] + '.pth'
-                #print(model)
+                # descargar imagen
+                r = requests.get('http://pinta.bicubi.co/get_next_to_process.php', headers=headers)
+                if r.status_code == 200:
+                    next_style = r.json()
+                    if next_style and not next_style['error']:
+                        print('-----------------------------------')
+                        r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_style['id'], 'status': 'PROCESANDO'}, headers=headers)
+                        print(str(time.time()), 'procesando imagen', next_style['name'])
+                        print('-----------------------------------')
 
-                style = next_style['estilo']
-                # guardar imagen para procesar
-                filename = 'process/' + str(time.time()) + '_' + next_style['name'] + next_style['ext']
-                with open(filename, "wb") as fh:
-                    fh.write(base64.b64decode(next_style['imagen']))
-                # ajustar
-                #image.fit_image(filename).save(filename)
+                        model = 'model/' + next_style['estilo'] + '.pth'
+                        #print(model)
 
-                # procesar imagen
-                path_styled = 'process/' + str(time.time()) + '_' + next_style['name'] + '_' + next_style['estilo'] + next_style['ext']           
-                os.system('python neural_style/neural_style.py eval --content-image ' + filename + ' --model ' + model + ' --output-image ' + path_styled + ' --cuda 1')
-                while not os.path.exists(path_styled):
-                    process = True
-                # styled con marco
-                #if not 'ppm_sm_pd' in filename:
-                #    image.printeable_image(path_styled).save(path_styled)
+                        style = next_style['estilo']
+                        # guardar imagen para procesar
+                        filename = 'process/' + str(time.time()) + '_' + next_style['name'] + next_style['ext']
+                        with open(filename, "wb") as fh:
+                            fh.write(base64.b64decode(next_style['imagen']))
+                        # ajustar
+                        #image.fit_image(filename).save(filename)
 
-                #TODO: cambiar por procesada
-                #imagen = filename
-                imagen = path_styled
-                files = {'file': open(imagen, 'rb')}
-                r = requests.post('http://pinta.bicubi.co/upload_style.php',
-                    files=files,
-                    data={'id': next_style['id'], 'name': next_style['name'] + '_' + next_style['estilo'] + next_style['ext']},
-                    headers=headers)
-                #print(r.json())
+                        # procesar imagen
+                        path_styled = 'process/' + str(time.time()) + '_' + next_style['name'] + '_' + next_style['estilo'] + next_style['ext']           
+                        os.system('python neural_style/neural_style.py eval --content-image ' + filename + ' --model ' + model + ' --output-image ' + path_styled + ' --cuda 1')
+                        while not os.path.exists(path_styled):
+                            process = True
+                        # styled con marco
+                        #if not 'ppm_sm_pd' in filename:
+                        #    image.printeable_image(path_styled).save(path_styled)
 
-                # borrar imagenes
-                #os.remove(filename)
-                #os.remove(path_styled)
+                        #TODO: cambiar por procesada
+                        #imagen = filename
+                        imagen = path_styled
+                        files = {'file': open(imagen, 'rb')}
+                        r = requests.post('http://pinta.bicubi.co/upload_style.php',
+                            files=files,
+                            data={'id': next_style['id'], 'name': next_style['name'] + '_' + next_style['estilo'] + next_style['ext']},
+                            headers=headers)
+                        #print(r.json())
 
-                print('-----------------------------------')
-                r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_style['id'], 'status': 'PROCESADO'}, headers=headers)
-                print(str(time.time()), 'procesada imagen', next_style['name'])
-                print('-----------------------------------')
+                        # borrar imagenes
+                        #os.remove(filename)
+                        #os.remove(path_styled)
+
+                        print('-----------------------------------')
+                        r = requests.post('http://pinta.bicubi.co/set_status.php', data={'id': next_style['id'], 'status': 'PROCESADO'}, headers=headers)
+                        print(str(time.time()), 'procesada imagen', next_style['name'])
+                        print('-----------------------------------')
+                    #if next_style and not next_style['error']:
+                    else:
+                        print(str(time.time()), next_style['message'])
+                #if r.status_code == 200:
+                else:
+                    print(str(time.time()), 'Error al recibir imagen desde el servidor')
+            #if next_style and not next_style['error']:
             else:
-                print(str(time.time()), 'no hay más por procesar')
-
+                print(str(time.time()), next_style['message'])
+        #if r.status_code == 200:
         else:
             print(str(time.time()), 'Error al recibir respuesta desde el servidor')
 
-    except:
-        print('Error red?')
+    except Exception as ex:
+        print('Error', ex)
     # do your stuff
     s.enter(10, 1, find_next_process, (sc,))
 
